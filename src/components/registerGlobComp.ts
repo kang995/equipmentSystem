14f0641ff073e50f63988d162e26b233dedd1a58
张建勋 <zhangjianxun@ts-it.cn>
2021-09-10 11:21:33 +0800

    perf: 配置自定义loading指示符

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -1,7 +1,19 @@
 import type { App } from 'vue';
 import { Button } from './Button';
-import { Input, Layout } from 'ant-design-vue';
+import { Input, Layout, Spin } from 'ant-design-vue';
 
+import { h } from 'vue';
+import { LoadingOutlined } from '@ant-design/icons-vue';
+// 全局配置自定义loading指示符
+// Spin.setDefaultIndicator(<PageLoading />);
+Spin.setDefaultIndicator({
+  indicator: h(LoadingOutlined, {
+    style: {
+      fontSize: '24px',
+    },
+    spin: true,
+  }),
+});
 export function registerGlobComp(app: App) {
   app.use(Input).use(Button).use(Layout);
 }
