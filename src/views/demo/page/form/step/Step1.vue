9e8397b16716ff1ce3cae597578b1fd11551bbb5
张建勋 <zhangjianxun@ts-it.cn>
2022-04-12 12:39:36 +0800

    fix: 修复a-form-item-rest问题；修复样式

--- src/views/demo/page/form/step/Step1.vue
+++ src/views/demo/page/form/step/Step1.vue
@@ -4,11 +4,13 @@
       <BasicForm @register="register">
         <template #fac="{ model, field }">
           <a-input-group compact>
-            <a-select v-model:value="model['pay']" class="pay-select">
-              <a-select-option value="zfb"> 支付宝 </a-select-option>
-              <a-select-option value="yl"> 银联 </a-select-option>
-            </a-select>
-            <a-input class="pay-input" v-model:value="model[field]" />
+            <a-form-item-rest>
+              <a-select v-model:value="model['pay']" class="pay-select" style="width: 30%">
+                <a-select-option value="zfb"> 支付宝 </a-select-option>
+                <a-select-option value="yl"> 银联 </a-select-option>
+              </a-select>
+            </a-form-item-rest>
+            <a-input class="pay-input" v-model:value="model[field]" style="width: 70%" />
           </a-input-group>
         </template>
       </BasicForm>
@@ -31,7 +33,7 @@
   import { BasicForm, useForm } from '/@/components/Form';
   import { step1Schemas } from './data';
 
-  import { Select, Input, Divider } from 'ant-design-vue';
+  import { Select, Input, Divider, Form } from 'ant-design-vue';
   export default defineComponent({
     components: {
       BasicForm,
@@ -40,6 +42,7 @@
       [Input.name]: Input,
       [Input.Group.name]: Input.Group,
       [Divider.name]: Divider,
+      AFormItemRest: Form.ItemRest,
     },
     emits: ['next'],
     setup(_, { emit }) {
@@ -93,11 +96,11 @@
     }
   }
 
-  .pay-select {
-    width: 20%;
-  }
-
-  .pay-input {
-    width: 70%;
-  }
+  //.pay-select {
+  //  width: 20%;
+  //}
+  //
+  //.pay-input {
+  //  width: 70%;
+  //}
 </style>
