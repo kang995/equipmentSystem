8fe49aef44c58c00f9314114d569ffe17b308029
张建勋 <zhangjianxun@ts-it.cn>
2022-04-28 17:27:58 +0800

    style: 开启消息通知图标

--- src/settings/projectSetting.ts
+++ src/settings/projectSetting.ts
@@ -70,7 +70,7 @@ const setting: ProjectConfig = {
     // Whether to show the document button
     showDoc: false,
     // Whether to show the notification button
-    showNotice: false,
+    showNotice: true,
     // Whether to display the menu search
     showSearch: false,
   },
