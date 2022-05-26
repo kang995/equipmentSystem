13bad31abc6a5a9751618d29890e468ceac0e244
张建勋 <zhangjianxun@ts-it.cn>
2022-05-26 09:47:38 +0800

    fix: 添加全局路由后置守卫，切换路由时，滚动到页面顶部

--- src/layouts/default/content/index.vue
+++ src/layouts/default/content/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div :class="[prefixCls, getLayoutContentMode]" v-loading="getOpenPageLoading && getPageLoading">
+  <div
+    :class="[prefixCls, getLayoutContentMode]"
+    v-loading="getOpenPageLoading && getPageLoading"
+    ref="layoutContent"
+  >
     <PageLayout />
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, ref } from 'vue';
   import PageLayout from '/@/layouts/page/index.vue';
   import { useDesign } from '/@/hooks/web/useDesign';
   import { useRootSetting } from '/@/hooks/setting/useRootSetting';
   import { useTransitionSetting } from '/@/hooks/setting/useTransitionSetting';
   import { useContentViewHeight } from './useContentViewHeight';
+  import { RouteLocationNormalized, useRouter } from 'vue-router';
 
   export default defineComponent({
     name: 'LayoutContent',
@@ -18,13 +23,28 @@
       const { prefixCls } = useDesign('layout-content');
       const { getOpenPageLoading } = useTransitionSetting();
       const { getLayoutContentMode, getPageLoading } = useRootSetting();
+      const layoutContent = ref();
+      const router = useRouter();
 
       useContentViewHeight();
+
+      const isHash = (href: string) => {
+        return /^#/.test(href);
+      };
+      // 切换页面，离开当前路由时滚动到顶部
+      router.afterEach(async (to) => {
+        // scroll top
+        isHash((to as RouteLocationNormalized & { href: string })?.href) &&
+          layoutContent.value.scrollTo(0, 0);
+        return true;
+      });
+
       return {
         prefixCls,
         getOpenPageLoading,
         getLayoutContentMode,
         getPageLoading,
+        layoutContent,
       };
     },
   });
