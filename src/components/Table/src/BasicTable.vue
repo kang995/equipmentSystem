073cc10b3ff59837116f8fbfc46d88338035d8a6
zxiaozhou <62740686+zxiaozhou@users.noreply.github.com>
2022-11-06 06:55:39 +0800

    表格添加一个setRows方法以便数据回显后重新获取数据 (#2327)

--- src/components/Table/src/BasicTable.vue
+++ src/components/Table/src/BasicTable.vue
@@ -139,6 +139,7 @@
         getRowSelection,
         getRowSelectionRef,
         getSelectRows,
+        setSelectedRows,
         clearSelectedRowKeys,
         getSelectRowKeys,
         deleteSelectRowByKey,
