5589a16bffac70347587f47f71fdcce93da2fc01
kang995 <978147028@qq.com>
2022-12-19 18:47:50 +0800

    feat: 新增设备拆除报废路由

---
+++ src/views/device-teardown/teardown-management/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div> 设备拆除管理 </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
---
+++ src/views/device-teardown/teardown-record/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div> 拆除记录 </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
